4a9396e3bb08e0d430e4638fe8b93acc5efca106
Hoang Dung <hoangdung0896@gmail.com>
2019-12-13 21:40:21 +0700

    update async in redux

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const AboutPage = () => (
-    <div>
-        <h2>Contact us</h2>
-    </div>
+  <div>
+    <h2>About</h2>
+    <p>
+      This app uses React, Redux, React Router, and many other helpful
+      libraries.
+    </p>
+  </div>
 );
 
 export default AboutPage;
