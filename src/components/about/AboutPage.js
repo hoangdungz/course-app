4374e808a572390f99952b979a455b9ef516e5cb
Hoang Dung <hoangdung0896@gmail.com>
2019-12-26 01:30:06 +0700

    handle async status

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const AboutPage = () => (
-    <div>
-        <h2>About</h2>
-        <p>Contact us!</p>
-    </div>
+  <div>
+    <h2>About</h2>
+    <p>
+      This app uses React, Redux, React Router, and many other helpful
+      libraries.
+    </p>
+  </div>
 );
 
 export default AboutPage;
