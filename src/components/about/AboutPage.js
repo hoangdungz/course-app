f6bebb516f7970f8e8908532fd6f6169663d1383
Hoang Dung <hoangdung0896@gmail.com>
2019-12-27 01:47:21 +0700

    testing react redux

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,13 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const AboutPage = () => (
-  <div>
-    <h2>About</h2>
-    <p>
-      This app uses React, Redux, React Router, and many other helpful
-      libraries.
-    </p>
-  </div>
+    <div>
+        <h2>About</h2>
+        <p>Contact us</p>
+    </div>
 );
 
 export default AboutPage;
